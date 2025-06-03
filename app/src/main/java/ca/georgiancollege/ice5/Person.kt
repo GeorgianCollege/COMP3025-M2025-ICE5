0035425119219fd96c980a15c4a3fd97c3bd1394
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2025-06-03 15:40:05 -0400

    added Person class

---
+++ app/src/main/java/ca/georgiancollege/ice5/Person.kt
@@ -0,0 +1,23 @@
+package ca.georgiancollege.ice5
+
+class Person (private var name: String, private var age: Float)
+{
+   // public properties
+   var Name: String
+      get() = name
+      set(value) {
+         if(value.isNotBlank()) name = value
+      }
+
+   var Age: Float
+      get() = age
+      set(value) {
+         if(value >= 0) age = value
+      }
+
+   // overrides
+   override fun toString(): String
+   {
+      return "Person(name='$name', age=$age)"
+   }
+}
